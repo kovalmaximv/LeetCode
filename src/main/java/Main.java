d4dcef728c16d9f5381a099254bab53e840861c2
Maxim Koval <kovalmaximv@gmail.com>
2020-07-15 20:11:15 +0300

    day3,4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
-import Heap.Easy.LastStoneWeight;
+import Array.Easy.TheKWeakestRowsInMatrix;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LastStoneWeight.lastStoneWeight(new int[]{3,7,10,11,2,1,8}));
+        System.out.println(
+                Arrays.toString(
+                        TheKWeakestRowsInMatrix
+                                .kWeakestRows(new int[][]{{1,1,1,0,0,0,0},{1,1,1,1,1,1,0},{0,0,0,0,0,0,0},{1,1,1,0,0,0,0},{1,1,1,1,1,1,1}}, 4)));
     }
 }
