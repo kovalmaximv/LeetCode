ead3fc8f4d97a65e4b82c56adfd5cf06bc5eb788
Maxim Koval <kovalmaximv@gmail.com>
2020-08-01 22:37:59 +0300

    dayN

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 import HashTable.Medium.AvoidFloodTheCity;
+import Strings.Medium.NumberGoodWaysSplitString;
 
 import java.util.Arrays;
 
+
 public class Main {
     public static void main(String[] args) {
-        AvoidFloodTheCity task = new AvoidFloodTheCity();
-        System.out.println(Arrays.toString(task.avoidFlood(
-                new int[]{2,3,0,0,3,1,0,1,0,2,2}
-        )));
+        NumberGoodWaysSplitString task = new NumberGoodWaysSplitString();
+        System.out.println(task.numSplits("aaaaa"));
     }
 }
---
+++ src/main/java/Strings/Medium/NumberGoodWaysSplitString.java
@@ -0,0 +1,48 @@
+package Strings.Medium;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+// https://leetcode.com/problems/number-of-good-ways-to-split-a-string/
+// Medium
+// Problem: 0
+// #NEEDSOULTION 02.08
+
+public class NumberGoodWaysSplitString {
+    public int numSplits(String s) {
+        Set<Character> setLeft = new HashSet<>();
+        Map<Character, Integer> setRight = toMap(s);
+
+        int count = 0;
+        for (char character: s.toCharArray()) {
+            setLeft.add(character);
+
+            int newCount = setRight.get(character) - 1;
+            if (newCount <= 0) {
+                setRight.remove(character);
+            } else {
+                setRight.put(character, newCount);
+            }
+            if (setLeft.size() == setRight.keySet().size()) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private Map<Character, Integer> toMap(String string) {
+        Map<Character, Integer> res = new HashMap<>();
+        for(char character: string.toCharArray()) {
+            if (res.containsKey(character)) {
+                res.put(character, res.get(character) + 1);
+            } else {
+                res.put(character, 1);
+            }
+        }
+
+        return res;
+    }
+}
