76cd4d72c4e9cf16d26b5ba2b816e48a9a187e09
Maxim Koval <kovalmaximv@gmail.com>
2020-09-06 21:15:53 +0300

    dayN

---
+++ src/main/java/LeetCode/Stack/Easy/MinStack.java
@@ -0,0 +1,44 @@
+package LeetCode.Stack.Easy;
+
+// https://leetcode.com/problems/min-stack/submissions/
+// Easy
+// Problem: 0 - ordinary solving, 1 - best solving
+
+public class MinStack {
+    private Node head;
+
+    public void push(int x) {
+        if(head == null)
+            head = new Node(x, x);
+        else
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
-import LeetCode.DynamicProgramming.Medium.UniquePaths;
+import LeetCode.Stack.Easy.MinStack;
 
 public class Main {
     public static void main(String[] args) {
-        UniquePaths task = new UniquePaths();
-        System.out.println(task.uniquePaths(1 ,1));
+        MinStack task = new MinStack();
+        task.push(2);
+        task.push(1);
+        task.push(5);
+        task.push(4);
+        System.out.println(task.getMin());
+        System.out.println(task.top());
+        task.pop();
+        System.out.println(task.getMin());
+        System.out.println(task.top());
     }
 }
